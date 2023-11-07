78434ddcb8ba3d0bc6259075a63cea0f39f641e5
Hong Phuc <hongphuc.dthp@gmail.com>
2023-11-08 01:20:40 +0700

    cypress: add first test

--- cypress/e2e/fundamentals.cy.js
+++ cypress/e2e/fundamentals.cy.js
@@ -1,5 +1,14 @@
-describe('template spec - update string', () => {
+const BASE_URL = "http://localhost:3000"
+
+describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit(`${BASE_URL}/fundamentals`);
+
+	// 1st approach
+	// cy.get('h1').should('have.text', 'Testing Fundamentals');
+
+	// 2nd approach
+	cy.get('[data-text="fundamentals-header"]').contains(/Testing Fundamentals/i);
+	// cy.get('[data-text="fundamentals-header"]').contains("Testing Fundamentals");
   })
 })
